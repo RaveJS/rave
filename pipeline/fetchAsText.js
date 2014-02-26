eed75f2207e347007131c25ea04cbe22785f910c
unscriptable <john@unscriptable.com>
2014-02-25 23:07:29 -0500

    Add node text fetching capabilities.

--- pipeline/fetchAsText.js
+++ pipeline/fetchAsText.js
@@ -3,10 +3,33 @@
 /** @author John Hann */
 module.exports = fetchAsText;
 
-var fetchText = require('../lib/fetchXhrText');
+// pre-load browser fetcher so it will be built in to rave.js
+var fetchXhrText = require('../lib/fetchXhrText');
+
+var env = detectEnv();
 
 function fetchAsText (load) {
-	return new Promise(function(resolve, reject) {
-		fetchText(load.address, resolve, reject);
+	// get fetchText on first use
+	var fetcher = getFetchText(env);
+	return Promise.resolve(fetcher).then(function (fetchText) {
+		return new Promise(function(resolve, reject) {
+			fetchText(load.address, resolve, reject);
+		});
 	});
+}
+
+function getFetchText (env) {
+	if (env === 'node') return require.async('../lib/fetchNodeText');
+	else return Promise.resolve(fetchXhrText);
+}
+
+function detectEnv () {
+	try {
+		// try to get the fs module since we're going to use it
+		global.require('fs');
+		return 'node';
+	}
+	catch (ex) {
+		return 'browser';
+	}
 }
--- rave.js
+++ rave.js
@@ -1858,12 +1858,35 @@ function normalizeCjs (name, refererName, refererUrl) {
 
 ;define('rave/pipeline/fetchAsText', ['require', 'exports', 'module', 'rave/lib/fetchXhrText'], function (require, exports, module, $cram_r0, define) {module.exports = fetchAsText;
 
-var fetchText = $cram_r0;
+// pre-load browser fetcher so it will be built in to rave.js
+var fetchXhrText = $cram_r0;
+
+var env = detectEnv();
 
 function fetchAsText (load) {
-	return new Promise(function(resolve, reject) {
-		fetchText(load.address, resolve, reject);
+	// get fetchText on first use
+	var fetcher = getFetchText(env);
+	return Promise.resolve(fetcher).then(function (fetchText) {
+		return new Promise(function(resolve, reject) {
+			fetchText(load.address, resolve, reject);
+		});
 	});
+}
+
+function getFetchText (env) {
+	if (env === 'node') return require.async('../lib/fetchNodeText');
+	else return Promise.resolve(fetchXhrText);
+}
+
+function detectEnv () {
+	try {
+		// try to get the fs module since we're going to use it
+		global.require('fs');
+		return 'node';
+	}
+	catch (ex) {
+		return 'browser';
+	}
 }
 
 });
