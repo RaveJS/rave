eb9c99943590670828712ae1dda0bf6118f0595b
unscriptable <john@unscriptable.com>
2014-02-27 23:00:24 -0500

    Use the environment sniffed at bootstrap time.  Sniffing later may sniff wrongly.

--- pipeline/fetchAsText.js
+++ pipeline/fetchAsText.js
@@ -6,11 +6,9 @@ module.exports = fetchAsText;
 // pre-load browser fetcher so it will be built in to rave.js
 var fetchXhrText = require('../lib/fetchXhrText');
 
-var env = detectEnv();
-
 function fetchAsText (load) {
 	// get fetchText on first use
-	var fetcher = getFetchText(env);
+	var fetcher = getFetchText(load.metadata.rave.environ);
 	return Promise.resolve(fetcher).then(function (fetchText) {
 		return new Promise(function(resolve, reject) {
 			fetchText(load.address, resolve, reject);
@@ -24,12 +22,4 @@ function getFetchText (env) {
 			? (require)('../lib/fetchNodeText')
 			: fetchXhrText
 	);
-}
-
-function detectEnv () {
-	var test
-	// try to get the fs module since we're going to use it
-	// we're using parens to hide `require` from cram.js
-	try { test = (require)('fs'); } catch (ex) {}
-	return test ? 'node' : 'browser';
 }
