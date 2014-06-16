e2753aa535e4d58c4810849e71381c3579e109b4
Karolis Narkevicius <karolis.n@gmail.com>
2014-06-16 22:32:32 +0100

    Move the uid parsing logic into createPackageMapper module

--- test/lib/createMapper.js
+++ test/lib/createMapper.js
@@ -8,32 +8,32 @@ var createMapper = require('../../lib/createMapper');
 buster.testCase('createMapper', {
 
 	'should create a function': function () {
-		var map = createMapper({}, function () {});
+		var map = createMapper({});
 		assert.isFunction(map);
 	},
 
 	'should not change the module id if there is no map': function () {
-		var packages = {'a': {}};
-		var map = createMapper({packages: packages}, function () {});
-		assert.equals(map('./some/path.js', 'a'), './some/path.js');
+		var packages = {'foo': {}};
+		var map = createMapper({packages: packages});
+		assert.equals(map('./some/path.js', 'foo'), './some/path.js');
 	},
 
 	'should not change the module id if it is not in the map': function () {
-		var packages = {'a': {map: {'./some/other/path': './foo'}}};
-		var map = createMapper({packages: packages}, function () {});
-		assert.equals(map('./some/path.js', 'a'), './some/path.js');
+		var packages = {'foo': {map: {'./some/other/path': './foo'}}};
+		var map = createMapper({packages: packages});
+		assert.equals(map('./some/path.js', 'foo'), './some/path.js');
 	},
 
 	'should replace the module id if it is in the map': function () {
-		var packages = {'a': {map: {'foo#foo/some/path': 'foo#foo/browser'}}};
+		var packages = {'foo': {map: {'foo/some/path': 'foo/browser'}}};
 		var map = createMapper({packages: packages});
-		assert.equals(map('foo#foo/some/path', 'a'), 'foo#foo/browser');
+		assert.equals(map('foo/some/path', 'foo'), 'foo/browser');
 	},
 
 	'should replace the module id with rave/lib/blank if it is mapped to false': function () {
-		var packages = {'a': {map: {'bar': false}}};
+		var packages = {'foo': {map: {'bar': false}}};
 		var map = createMapper({packages: packages});
-		assert.equals(map('bar', 'a'), 'rave/lib/blank');
+		assert.equals(map('bar', 'foo'), 'rave/lib/blank');
 	}
 
 });
